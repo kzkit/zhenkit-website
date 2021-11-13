c9972986b3a0ea94f8074447e912fed5feaf1eb7
Zhen Kit <zhenkit1996@gmail.com>
2021-11-13 21:16:31 +0800

    added responsive css

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,29 @@
 <template>
-	<div class="h-screen bg-gray-900">
-		<div class="mx-auto w-full max-w-2xl space-y-2 py-8 justify-center">
-			<Navigation />
-			<h1 class="text-white sm:text-lg md:text-xl lg:text-4xl">
-				Kong Zhen Kit
-			</h1>
-			<p class="text-white pt-5 sm:text-md md:text-lg lg:text-xl">
-				Developer & Web 3 Enthusiast 🚀
-			</p>
-			<p class="text-white pt-5">
-				Exploring Solidity and smart contracts. Based in Kuala Lumpur, MY.
-			</p>
-			<NFT />
+	<div class="h-screen min-h-full flex flex-col">
+		<div
+			class="
+				font-sans
+				bg-gray-900
+				text-white
+				min-h-full
+				flex flex-col flex-grow
+				transition-colors
+				duration-200
+			"
+		>
+			<div class="mx-auto w-full max-w-2xl space-y-2 p-4">
+				<Navigation />
+				<h1 class="text-white sm:text-lg md:text-xl lg:text-4xl">
+					Kong Zhen Kit
+				</h1>
+				<p class="text-white pt-5 sm:text-md md:text-lg lg:text-xl">
+					Developer & Web 3 Enthusiast 🚀
+				</p>
+				<p class="text-white pt-5">
+					Exploring Solidity and smart contracts. Based in Kuala Lumpur, MY.
+				</p>
+				<NFT />
+			</div>
 		</div>
 	</div>
 </template>
