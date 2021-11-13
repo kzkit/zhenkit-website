5af069d81ceadb098a2b08d10049f475af4254d3
Zhen Kit <zhenkit1996@gmail.com>
2021-11-13 20:43:10 +0800

    added 404 view

---
+++ src/views/NotFound.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="h-screen bg-gray-900">
+		<div
+			class="container mx-auto px-96 sm:py-24 lg:py-48 items-center text-center"
+		>
+			<h1 class="text-white sm:text-lg md:text-xl lg:text-4xl">
+				404 Not Found
+			</h1>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
