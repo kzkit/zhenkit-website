d4390f6a0ec73d34e4ddc0bc24ee75a8caac0145
Zhen Kit <zhenkit1996@gmail.com>
2021-11-13 20:44:35 +0800

    added urls for socmed and github

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -7,7 +7,9 @@
 			@zhenkitkong
 		</a>
 		<div class="flex-grow"></div>
-		<a href="" class="text-white hover:text-green-500"
+		<a
+			href="https://twitter.com/zhenkitkong"
+			class="text-white hover:text-green-500"
 			><svg
 				xmlns="http://www.w3.org/2000/svg"
 				width="1em"
@@ -24,7 +26,7 @@
 					d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"
 				></path></svg
 		></a>
-		<a class="text-white hover:text-green-500" href="">
+		<a class="text-white hover:text-green-500" href="https://github.com/kzkit/">
 			<svg
 				xmlns="http://www.w3.org/2000/svg"
 				width="1em"
