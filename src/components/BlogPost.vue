2a9c2ecd5c33fa963fabcaed92a2977842971fcd
Zhen Kit <zhenkit1996@gmail.com>
2021-12-27 17:14:42 +0800

    added padding and formatted blog date

--- src/components/BlogPost.vue
+++ src/components/BlogPost.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="flex flex-col pt-8">
 		<h2 class="text-white sm:text-md md:text-lg lg:text-xl pb-5">Blog Posts</h2>
-		<div v-for="(post, index) in feedData" :key="index">
+		<div class="mb-4" v-for="(post, index) in feedData" :key="index">
 			<a
 				:href="post.link"
 				target="_blank"
@@ -18,28 +18,35 @@
 </template>
 
 <script>
-import { ref } from 'vue'
-import { onMounted } from '@vue/runtime-core'
+	import { ref } from 'vue'
+	import { onMounted } from '@vue/runtime-core'
 
-export default {
-	setup() {
-		const feedData = ref([])
+	export default {
+		setup() {
+			const feedData = ref([])
 
-		const getFeed = () => {
-			return fetch(
-				'https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40zhenkit'
-			).then((response) => response.json())
-		}
-		onMounted(async () => {
-			const data = await getFeed()
-			feedData.value = data.items
-		})
-		return {
-			feedData,
-		}
-	},
-}
+			const getFeed = () => {
+				return fetch(
+					'https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40zhenkit'
+				).then((response) => response.json())
+			}
+
+			const convertDate = (items) => {
+				for (let item of items) {
+					if (item) {
+						let blogDate = new Date(item.pubDate)
+						item.pubDate = blogDate.toLocaleDateString()
+					}
+				}
+			}
+			onMounted(async () => {
+				const data = await getFeed()
+				feedData.value = data.items
+				convertDate(feedData.value)
+			})
+			return {
+				feedData,
+			}
+		},
+	}
 </script>
-
-<style>
-</style>
